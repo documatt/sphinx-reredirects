1ad8d01b714914e65f0099e0dfd96e602f6c6ac6
Libor Jelínek <liborjelinek@documatt.com>
2021-04-22 11:56:55 +0200

    remove accidentaly adding Sphinx theme in setup.py

--- setup.py
+++ setup.py
@@ -10,12 +10,6 @@ setup(
     description='Handles redirects for moved pages in Sphinx documentation projects',
     long_description=open('README.rst', encoding='utf-8').read(),
     packages=['sphinx_reredirects'],
-    # See http://www.sphinx-doc.org/en/stable/theming.html#distribute-your-theme-as-a-python-package
-    entry_points={
-        'sphinx.html_themes': [
-            'sphinx_documatt_theme = sphinx_documatt_theme',
-        ]
-    },
     install_requires=[
         'sphinx'
     ],
