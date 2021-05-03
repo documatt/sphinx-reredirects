c3ad68ef46b1186498c519fffb7c5402fdac8ae9
Libor Jelínek <liborjelinek@documatt.com>
2021-05-03 12:39:42 +0200

    add tox

--- tests/test_wildcards.py
+++ tests/test_wildcards.py
@@ -5,13 +5,11 @@ from sphinx_reredirects import Reredirects
 
 def test_wildcard_expansion():
     sphinx_mock = Mock()
-    sphinx_mock.config = {
-        "redirects": {
-            "faq/*": "http://new.com/faq/",
-            "welcome": "intro.html"
-        },
-        "redirect_html_template_file": None
+    sphinx_mock.config.redirects = {
+        "faq/*": "http://new.com/faq/",
+        "welcome": "intro.html"
     }
+    sphinx_mock.config.redirect_html_template_file = None
     sphinx_mock.env.found_docs = ["faq/one", "faq/two", "intro"]
     actual = Reredirects(sphinx_mock).grab_redirects()
     expected = {
