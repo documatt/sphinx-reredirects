99c10c5842a31a14558537da0d495038fb0b3aeb
Libor Jelínek <liborjelinek@documatt.com>
2024-06-21 07:29:23 +0200

    ref: format with ruff

--- tests/test_end2end.py
+++ tests/test_end2end.py
@@ -21,8 +21,9 @@ def test_ext(app: Sphinx, status, warning):
     status = status.getvalue()
 
     assert (
-        app.outdir / "faq/one.html"
-    ).read_text() == '<html><head><meta http-equiv="refresh" content="0; url=https://new.com/faq/one.html"></head></html>'  # noqa: E501
+        (app.outdir / "faq/one.html").read_text()
+        == '<html><head><meta http-equiv="refresh" content="0; url=https://new.com/faq/one.html"></head></html>'
+    )  # noqa: E501
 
     assert (
         """Overwriting 'faq/one.html' with redirect to 'https://new.com/faq/one.html'."""  # noqa: E501
@@ -30,8 +31,9 @@ def test_ext(app: Sphinx, status, warning):
     )
 
     assert (
-        app.outdir / "faq/two.html"
-    ).read_text() == '<html><head><meta http-equiv="refresh" content="0; url=https://new.com/faq/two.html"></head></html>'  # noqa: E501
+        (app.outdir / "faq/two.html").read_text()
+        == '<html><head><meta http-equiv="refresh" content="0; url=https://new.com/faq/two.html"></head></html>'
+    )  # noqa: E501
 
     assert (
         """Overwriting 'faq/two.html' with redirect to 'https://new.com/faq/two.html'."""  # noqa: E501
@@ -39,8 +41,9 @@ def test_ext(app: Sphinx, status, warning):
     )
 
     assert (
-        app.outdir / "install.html"
-    ).read_text() == '<html><head><meta http-equiv="refresh" content="0; url=go-to-install"></head></html>'  # noqa: E501
+        (app.outdir / "install.html").read_text()
+        == '<html><head><meta http-equiv="refresh" content="0; url=go-to-install"></head></html>'
+    )  # noqa: E501
 
     assert (
         """Overwriting 'install.html' with redirect to 'go-to-install'."""  # noqa: E501
@@ -48,14 +51,16 @@ def test_ext(app: Sphinx, status, warning):
     )
 
     assert (
-        app.outdir / "setup.html"
-    ).read_text() == '<html><head><meta http-equiv="refresh" content="0; url=install.html"></head></html>'  # noqa: E501
+        (app.outdir / "setup.html").read_text()
+        == '<html><head><meta http-equiv="refresh" content="0; url=install.html"></head></html>'
+    )  # noqa: E501
 
     assert """Creating redirect 'setup.html' to 'install.html'.""" in status
 
     assert (
-        app.outdir / "install/requirements.html"
-    ).read_text() == '<html><head><meta http-equiv="refresh" content="0; url=https://web.com/docs/requirements.html"></head></html>'  # noqa: E501
+        (app.outdir / "install/requirements.html").read_text()
+        == '<html><head><meta http-equiv="refresh" content="0; url=https://web.com/docs/requirements.html"></head></html>'
+    )  # noqa: E501
 
     assert (
         """Creating redirect 'install/requirements.html' to 'https://web.com/docs/requirements.html'."""  # noqa: E501
@@ -83,8 +88,9 @@ def test_dirhtml(app: Sphinx, status, warning):
     status = status.getvalue()
 
     assert (
-        app.outdir / "index.html"
-    ).read_text() == '<html><head><meta http-equiv="refresh" content="0; url=http://new.com/index"></head></html>'  # noqa: E501
+        (app.outdir / "index.html").read_text()
+        == '<html><head><meta http-equiv="refresh" content="0; url=http://new.com/index"></head></html>'
+    )  # noqa: E501
 
     assert (
         """Overwriting 'index.html' with redirect to '/newindex/'."""  # noqa: E501
@@ -97,8 +103,9 @@ def test_dirhtml(app: Sphinx, status, warning):
     )
 
     assert (
-        app.outdir / "install/index.html"
-    ).read_text() == '<html><head><meta http-equiv="refresh" content="0; url=/installing.html"></head></html>'  # noqa: E501
+        (app.outdir / "install/index.html").read_text()
+        == '<html><head><meta http-equiv="refresh" content="0; url=/installing.html"></head></html>'
+    )  # noqa: E501
 
     assert (
         """Creating redirect 'install/index.html' to '/installing/trailingslash'."""  # noqa: E501
@@ -136,9 +143,7 @@ def test_invalid_uri(app: Sphinx, status, warning):
     with pytest.raises(ExtensionError) as excinfo:
         app.build()
 
-    assert "for event 'html-collect-pages' threw an exception" in str(
-        excinfo.value
-    )  # noqa: E501
+    assert "for event 'html-collect-pages' threw an exception" in str(excinfo.value)  # noqa: E501
     assert any(
         [
             # Newer python versions:
