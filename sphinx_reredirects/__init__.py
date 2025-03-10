36c6a8bb7126d5e3d28c69525bb4747da744e30b
davidekete <eketedavid@gmail.com>
2025-03-10 08:16:07 +0100

    feat: update default HTML template to preserve url fragments

--- sphinx_reredirects/__init__.py
+++ sphinx_reredirects/__init__.py
@@ -17,7 +17,7 @@ OPTION_TEMPLATE_FILE = "redirect_html_template_file"
 OPTION_TEMPLATE_FILE_DEFAULT = None
 
 REDIRECT_FILE_DEFAULT_TEMPLATE = (
-    '<html><head><meta http-equiv="refresh" content="0; url=${to_uri}"></head></html>'  # noqa: E501
+    '<html><head><noscript><meta http-equiv="refresh" content="0; url=${to_uri}" /></noscript><script>var target = "${to_uri}";if (window.location.hash) {window.location.replace(target + window.location.hash);} else {window.location.replace(target);}</script></head></html>'  # noqa: E501
 )
 
 logger = logging.getLogger(__name__)
