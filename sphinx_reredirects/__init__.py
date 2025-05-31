97389aa5f568f1975ff7723d12d6a270cc6338d9
Libor Jelínek <liborjelinek@documatt.com>
2025-05-31 15:42:50 +0200

    chore: release v1.0.0

--- sphinx_reredirects/__init__.py
+++ sphinx_reredirects/__init__.py
@@ -1,6 +1,6 @@
 """The extension for Sphinx documentation projects that handle redirects for moved pages. It generates HTML pages with meta refresh redirects to the new page location to prevent 404 errors if you rename or move your documents."""
 
-__version__ = "1.0.0rc1"
+__version__ = "1.0.0"
 
 import re
 from fnmatch import fnmatch
